19b72b6f30d319ee81503fd50092f1c2fbb6ce18
Sebastian Hermida <sebas00@gmail.com>
2015-11-26 12:52:19 +0100

    Adding params to the callback

--- app.rb
+++ app.rb
@@ -9,7 +9,12 @@ module DnsimpleHeroku
         headers({ "X-Frame-Options" => "ALLOWALL" })
     end
 
-    get '/:account_id' do
+    get "/callback" do
+      @params = params
+      haml "callback"
+    end
+
+    get "/:account_id" do
 
       redirect "http://localhost:3000/oauth/authorize?client_id=#{CLIENT_ID}&response_type=code&state=1234567"
 
