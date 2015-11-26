eb1893073551d1b57b178fb37a17a3dae8017231
Sebastian Hermida <sebas00@gmail.com>
2015-11-26 12:41:21 +0100

    Allows all for iframes

--- app.rb
+++ app.rb
@@ -5,6 +5,10 @@ module DnsimpleHeroku
 
     CLIENT_ID = "637370e33040bf54"
 
+    after do
+        headers({ "X-Frame-Options" => "ALLOWALL" })
+    end
+
     get '/:account_id' do
 
       redirect "http://localhost:3000/oauth/authorize?client_id=#{CLIENT_ID}&response_type=code&state=1234567"
