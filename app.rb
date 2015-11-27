93e6d5522c1a94e76c3d9adb131126d375c6a45d
Sebastian Hermida <sebas00@gmail.com>
2015-11-27 11:13:08 +0100

    Httparty all the things

--- app.rb
+++ app.rb
@@ -1,9 +1,11 @@
 require 'sinatra/base'
+require 'httparty'
 
 module DnsimpleHeroku
   class App < Sinatra::Base
 
     CLIENT_ID = "637370e33040bf54"
+    CLIENT_SECRET = "Rxb0fPyiCY2onYRyUStaZvwZww8SzgaB"
 
     after do
         headers({ "X-Frame-Options" => "ALLOWALL" })
@@ -12,14 +14,25 @@ module DnsimpleHeroku
     get "/callback" do
       @params = params
       token = params[:code]
-      @output = `curl http://localhost:3000/v2/1/contacts?_api=1 -H "Authorization: #{token}"`
+
+      options = { client_id: CLIENT_ID,
+                  client_secret: CLIENT_SECRET,
+                  code: params[:code],
+                  redirect_uri: "http://fast-wave-9818.herokuapp.com/access_token",
+                  state: "1234567" }
+
+      response = HTTParty.get("http://localhost:3000/oauth/access_token",
+                             query: options)
       haml :callback
     end
 
     get "/" do
-
       redirect "http://localhost:3000/oauth/authorize?client_id=#{CLIENT_ID}&response_type=code&state=1234567"
+    end
 
+    get "/access_token" do
+      @params = params
+      haml :access_token
     end
 
   end
