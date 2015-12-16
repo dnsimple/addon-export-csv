87b5faa5eed0eedc23f2aba2e4420296aedbf756
Javier Acero <j4cegu@gmail.com>
2015-12-16 14:13:47 +0100

    Scope web app under the CsvExport namespace

--- web/app.rb
+++ web/app.rb
@@ -4,64 +4,66 @@ require_relative '../lib/account'
 require_relative '../lib/account_storage'
 require_relative '../lib/api_client'
 
-class CsvExportAddon < Sinatra::Base
+module CsvExport
+  class App < Sinatra::Base
 
-  before do
-    @accounts   = RedisAccountStorage.new
-    @api_client = ApiClient.new(
-      ENV["API_ENDPOINT"],
-      ENV["API_PORT"],
-      ENV["CLIENT_ID"],
-      ENV["CLIENT_SECRET"]
-    )
-  end
+    before do
+      @accounts   = RedisAccountStorage.new
+      @api_client = ApiClient.new(
+        ENV["API_ENDPOINT"],
+        ENV["API_PORT"],
+        ENV["CLIENT_ID"],
+        ENV["CLIENT_SECRET"]
+      )
+    end
 
-  get "/domains/:account_id/csv" do
-    account = @accounts.get(params[:account_id]) or halt 403
-    @domains = @api_client.domains(account.id, account.access_token)
+    get "/domains/:account_id/csv" do
+      account = @accounts.get(params[:account_id]) or halt 403
+      @domains = @api_client.domains(account.id, account.access_token)
 
-    haml :csv
-  end
+      haml :csv
+    end
 
-  post "/domains/:account_id/csv" do
-    account = @accounts.get(params[:account_id]) or halt 403
-    domains = @api_client.domains(account.id, account.access_token)
+    post "/domains/:account_id/csv" do
+      account = @accounts.get(params[:account_id]) or halt 403
+      domains = @api_client.domains(account.id, account.access_token)
 
-    content_type "application/csv"
+      content_type "application/csv"
 
-    CSV.generate(headers: ["Name", "State", "Expiration", "Whois privacy", "Auto renewal"],
-                 write_headers: true) do |csv|
-      domains.each do |domain|
-        csv << [
-          domain["name"],
-          domain["state"],
-          domain["expires_on"],
-          domain["private_whois"],
-          domain["auto_renew"]
-        ]
+      CSV.generate(headers: ["Name", "State", "Expiration", "Whois privacy", "Auto renewal"],
+                   write_headers: true) do |csv|
+        domains.each do |domain|
+          csv << [
+            domain["name"],
+            domain["state"],
+            domain["expires_on"],
+            domain["private_whois"],
+            domain["auto_renew"]
+          ]
+        end
       end
     end
-  end
 
 
-  get "/callback" do
-    auth = @api_client.authorization(params[:code])
+    get "/callback" do
+      auth = @api_client.authorization(params[:code])
 
-    @accounts.store(Account.new(auth.account_id, auth.access_token))
+      @accounts.store(Account.new(auth.account_id, auth.access_token))
 
-    redirect "/domains/#{auth.account_id}/csv"
-  end
+      redirect "/domains/#{auth.account_id}/csv"
+    end
 
-  get "/:account_id" do
-    if account = @accounts.get(params[:account_id])
-      redirect "/domains/#{account.id}/csv"
-    else
+    get "/:account_id" do
+      if account = @accounts.get(params[:account_id])
+        redirect "/domains/#{account.id}/csv"
+      else
+        haml :index
+      end
+    end
+
+    get "/" do
       haml :index
     end
-  end
 
-  get "/" do
-    haml :index
   end
-
 end
