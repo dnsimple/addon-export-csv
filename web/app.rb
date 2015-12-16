6c619d43bc405faeae88e28240be8c0d031d6706
Javier Acero <j4cegu@gmail.com>
2015-12-16 16:29:47 +0100

    Extract authentication related code to module

--- web/app.rb
+++ web/app.rb
@@ -1,35 +1,11 @@
 require 'sinatra/base'
-require 'warden'
 
+require_relative 'authentication'
 require_relative '../lib/csv_export'
 
 module CsvExport
   class App < Sinatra::Base
-
-    enable :sessions
-
-    use Warden::Manager do |manager|
-      manager.default_strategies :default
-      manager.failure_app = self
-    end
-
-    Warden::Strategies.add(:default) do
-      def authenticate!
-        account = CsvExport.account_service.get_account(session[:account_id])
-        success!(account)
-      rescue CsvExport::Errors::NotFound
-        fail!
-      end
-    end
-
-    Warden::Manager.serialize_into_session do |account|
-      account.id
-    end
-
-    Warden::Manager.serialize_from_session do |account_id|
-      CsvExport.account_service.get_account(account_id)
-    end
-
+    include Authentication
 
     get "/domains" do
       env["warden"].authenticate!
