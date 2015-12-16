ff9f01f66c3c0ae6b29ffbbd19b45ffc4bb94528
Javier Acero <j4cegu@gmail.com>
2015-12-16 16:17:44 +0100

    Use warden for authentication spike

--- web/app.rb
+++ web/app.rb
@@ -1,19 +1,49 @@
 require 'sinatra/base'
+require 'warden'
 
 require_relative '../lib/csv_export'
 
 module CsvExport
   class App < Sinatra::Base
 
-    get "/domains/:account_id/csv" do
-      account = CsvExport.account_service.get_account(params[:account_id]) or halt 403
+    enable :sessions
+
+    use Warden::Manager do |manager|
+      manager.default_strategies :default
+      manager.failure_app = self
+    end
+
+    Warden::Strategies.add(:default) do
+      def authenticate!
+        account = CsvExport.account_service.get_account(session[:account_id])
+        success!(account)
+      rescue CsvExport::Errors::NotFound
+        fail!
+      end
+    end
+
+    Warden::Manager.serialize_into_session do |account|
+      account.id
+    end
+
+    Warden::Manager.serialize_from_session do |account_id|
+      CsvExport.account_service.get_account(account_id)
+    end
+
+
+    get "/domains" do
+      env["warden"].authenticate!
+      account = env["warden"].user
+
       @domains = CsvExport.account_service.get_account_domains(account.id)
 
       haml :csv
     end
 
-    post "/domains/:account_id/csv" do
-      account = CsvExport.account_service.get_account(params[:account_id]) or halt 403
+    post "/domains" do
+      env["warden"].authenticate!
+      account = env["warden"].user
+
       domains = CsvExport.account_service.get_account_domains(account.id)
 
       content_type "application/csv"
@@ -34,18 +64,31 @@ module CsvExport
 
 
     get "/callback" do
-      CsvExport.account_service.authenticate_account(params[:code])
+      account = CsvExport.account_service.authenticate_account(params[:code])
+      session["account_id"] = account.id
 
-      redirect "/domains/#{auth.account_id}/csv"
+      redirect "/domains"
+    end
+
+    get "/unauthenticated" do
+      redirect "/login"
+    end
+
+    post "/unauthenticated" do
+      redirect "/login"
+    end
+
+    get "/login" do
+      redirect CsvExport.api_client.authorize_url
+    end
+
+    get "/logout" do
+      env["warden"].logout
+      redirect "/"
     end
 
     get "/:account_id" do
-      begin
-        account = CsvExport.account_service.get_account(params[:account_id])
-        redirect "/domains/#{account.id}/csv"
-      rescue CsvExport::Errors::NotFound
-        haml :index
-      end
+      env["warden"].authenticate!
     end
 
     get "/" do
