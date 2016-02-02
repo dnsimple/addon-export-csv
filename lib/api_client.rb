67e95a5643ba3591f47009a1996e821dadc096da
Javier Acero <j4cegu@gmail.com>
2016-02-02 10:57:29 +0100

    Some debugging

--- lib/api_client.rb
+++ lib/api_client.rb
@@ -15,6 +15,7 @@ class ApiClient
   def authorization(code)
     body = { client_id: @client_id, client_secret: @client_secret, code: code, state: "1234567" }
     response = HTTParty.post("#{@url}:#{@port}/oauth/access_token", body: body)
+    debug response
 
     if response.success?
       Auth.new(response["account_id"].to_s, response["access_token"].to_s)
@@ -26,8 +27,17 @@ class ApiClient
   def domains(account_id, access_token)
     headers  = { "Authorization" => "Bearer #{access_token}" }
     response = HTTParty.get("#{@url}:#{@port}/v2/#{account_id}/domains", headers: headers)
+    debug response
     response["data"]
   end
 
   Auth = Struct.new(:account_id, :access_token)
+
+  private
+
+  def debug(message)
+    STDERR.puts '#'*30
+    STDERR.puts message
+    STDERR.puts '#'*30
+  end
 end
