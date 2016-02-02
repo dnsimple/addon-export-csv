02fe24477a4e6049613a710e56b450126fc926df
Javier Acero <j4cegu@gmail.com>
2016-02-02 11:16:43 +0100

    Authorization is not under api subdomain

--- lib/api_client.rb
+++ lib/api_client.rb
@@ -1,21 +1,20 @@
 require 'httparty'
 
 class ApiClient
-  def initialize(url, port, client_id, client_secret)
-    @url = url
+  def initialize(host, port, client_id, client_secret)
+    @host = host
     @port = port
     @client_id = client_id
     @client_secret = client_secret
   end
 
   def authorize_url
-    "#{@url}:#{@port}/oauth/authorize?client_id=#{@client_id}&response_type=code&state=1234567"
+    "http://#{@host}:#{@port}/oauth/authorize?client_id=#{@client_id}&response_type=code&state=1234567"
   end
 
   def authorization(code)
     body = { client_id: @client_id, client_secret: @client_secret, code: code, state: "1234567" }
-    response = HTTParty.post("#{@url}:#{@port}/oauth/access_token", body: body)
-    debug response
+    response = HTTParty.post("http://#{@host}:#{@port}/oauth/access_token", body: body)
 
     if response.success?
       Auth.new(response["account_id"].to_s, response["access_token"].to_s)
@@ -26,18 +25,10 @@ class ApiClient
 
   def domains(account_id, access_token)
     headers  = { "Authorization" => "Bearer #{access_token}" }
-    response = HTTParty.get("#{@url}:#{@port}/v2/#{account_id}/domains", headers: headers)
-    debug response
+    response = HTTParty.get("http://api.#{@host}:#{@port}/v2/#{account_id}/domains", headers: headers)
     response["data"]
   end
 
   Auth = Struct.new(:account_id, :access_token)
 
-  private
-
-  def debug(message)
-    STDERR.puts '#'*30
-    STDERR.puts message
-    STDERR.puts '#'*30
-  end
 end
